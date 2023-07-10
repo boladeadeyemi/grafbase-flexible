25edddc7ba96a42c2d349f1dea58f0fde11b9d15
Bolade Adeyemi <boladeadeyemi@gmail.com>
2023-07-10 16:58:09 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import "./globals.css";
+
+export const metadata = {
+  title: "Flexibble",
+  description: "Showcase and discover remarkable developer projects",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <NavBar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
